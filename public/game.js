3bf0fdaeafa9e01458ae0c5fce7926d66d1a3a30
Bhavik Yakkarnall <bhavikyakkarnall@gmail.com>
2023-08-14 18:13:31 +1200

    Taken out delete button

--- public/game.js
+++ public/game.js
@@ -48,10 +48,9 @@ const displayGames = (games) => {
                         <p style="color:grey; margin-top: 10px;">Publisher: ${game.publisher}</p>
                     </div>
                     <div class="col-sm-12 col-md-2 d-flex justify-content-center align-items-center"> 
-                        <a id="" href="${game.game_url}" style="margin: 5px;" class="game-url btn btn-success"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAg0lEQVR4nO2SMQqAMAxF36hX0lGH3k7F6zlVHPQScckgolRoMgh98KEE+h+khYIRFTAAm2bQmRkTILeMloJDSxug1fPuIWi9BKP3impgvpTPOjNHNG7IbwQ9sGg6S0EA1ocfIx8TteOVmFEuF8kruSuQ1P0ikNSK3B85ZEpi6psWuHMCIyR3EebGVo8AAAAASUVORK5CYII="></a> 
-                        <a id="" href="#" style="margin: 5px;" class="game-url btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"></path>
-                        </svg></a> 
+                        <a id="" href="${game.game_url}" style="margin: 5px;" class="game-url btn btn-success">
+                            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAg0lEQVR4nO2SMQqAMAxF36hX0lGH3k7F6zlVHPQScckgolRoMgh98KEE+h+khYIRFTAAm2bQmRkTILeMloJDSxug1fPuIWi9BKP3impgvpTPOjNHNG7IbwQ9sGg6S0EA1ocfIx8TteOVmFEuF8kruSuQ1P0ikNSK3B85ZEpi6psWuHMCIyR3EebGVo8AAAAASUVORK5CYII=">
+                        </a> 
                     </div>
                 </div>
             </li>
